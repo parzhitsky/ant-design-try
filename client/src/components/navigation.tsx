a9d5b3ea8d24c7bdfeeef3692328078459925ac8
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-04-03 15:50:35 +0300

    Fix navigation item autoselect

--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -1,15 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Menu } from "antd";
 
+/** @private */
+const items = [
+	{
+		path: "/user",
+		label: "User page",
+	},
+	{
+		path: "/private",
+		label: "Private page",
+	},
+] as const;
+
 export default function Navigation() {
+	const { pathname } = useLocation<{}>();
+
 	return (
-		<Menu theme="dark" mode="horizontal">
-			<Menu.Item>
-				<Link to="/user">User page</Link>
-			</Menu.Item>
-			<Menu.Item>
-				<Link to="/private">Private page</Link>
-			</Menu.Item>
+		<Menu theme="dark" mode="horizontal" selectedKeys={[pathname]}>
+			{items.map((item) => (
+				<Menu.Item key={item.path}>
+					<Link to={item.path}>{item.label}</Link>
+				</Menu.Item>
+			))}
 		</Menu>
 	);
 }
