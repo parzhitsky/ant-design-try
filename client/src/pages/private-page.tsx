bdb877fb8eb8662a6d26b9ba74d563e07c8e001a
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-04-03 19:17:39 +0300

    Implement <PrivatePage />

--- client/src/pages/private-page.tsx
+++ client/src/pages/private-page.tsx
@@ -1,9 +1,22 @@
+import { Row, Col, Typography } from "antd";
 import AbstractPage from "./abstract-page";
 
 export default function PrivatePage() {
 	return (
-		<AbstractPage breadcrumbItem="Private">
-			PrivatePage
+		<AbstractPage authed breadcrumbItem="Private">
+			<Row justify="center">
+				<Col span={16}>
+					<Typography.Title level={3}>
+						Private page
+					</Typography.Title>
+					<Typography.Paragraph>
+						The content of this page is available only to logged-in users. 🤫
+					</Typography.Paragraph>
+					<Typography.Paragraph>
+						If you can see this message, you are logged-in, congratulations! 🎉🥳
+					</Typography.Paragraph>
+				</Col>
+			</Row>
 		</AbstractPage>
 	);
 }
