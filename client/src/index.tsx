e779c6cf85f2c3116acb6f8d5869eae4bac50900
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-04-05 00:10:23 +0300

    Use lowercase file names
    This repeats a2596bb and reverts 560b3b6

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import './index.css';
-import App from './App';
+import App from './app';
 import store from './store';
 
 ReactDOM.render(
